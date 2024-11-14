e19f661db81097cefd266884aad023c1d912b695
Zohar García <garciabrisa2007@gmail.com>
2024-11-14 10:13:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 // Redirigir de Login a Registro
-document.addEventListener("DOMContentLoaded", function() 
-{
+document.addEventListener("DOMContentLoaded", function() {
     const registerLink = document.querySelector(".register-link");
-    if (registerLink) 
-    {
-        registerLink.addEventListener("click", function(event) 
-        {
+    if (registerLink) {
+        registerLink.addEventListener("click", function(event) {
             event.preventDefault();
-            window.location.href = "registro.html";
+            window.location.href = "Registro.Html";
         });
     }
     
 // Redirigir de Registro a Login
     const loginLink = document.querySelector(".login-link");
-    if (loginLink) 
-    {
-        loginLink.addEventListener("click", function(event) 
-        {
+    if (loginLink) {
+        loginLink.addEventListener("click", function(event) {
             event.preventDefault();
-            window.location.href = "login.html";
+            window.location.href = "Login.Html";
         });
     }
 });
